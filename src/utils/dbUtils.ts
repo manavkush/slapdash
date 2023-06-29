d7592adbb0b53a9f62a61f9b2625edad3fc1d2bb
manavkush <manavkush@gmail.com>
2023-06-30 01:48:21 +0530

    feat: added the functionality to authorize() - tested insertToDB - tested AuthenticateUser - tested getUserInfoFromDB
    Co-authored-by: Saloni Singh <SaloniSingh1601@users.noreply.github.com>

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -1,43 +1,37 @@
 import { PrismaClient, User } from "@prisma/client";
-import {hash} from "bcrypt";
+import { hash, compare } from "bcrypt";
+import { profile } from "console";
 
 const prisma = new PrismaClient();
 
-interface TypeCheckUserPresentInDB {
-    isAuthenticated : boolean,
-    message: string,
-    data: object|null
-}
-export async function authenticateUser(username: string, password: string){
-    const passwordHashed = await hash(password,12);
+// TODO: Add a return type for the function
+export async function authenticateUser(username: string, password: string) {
     const user = await prisma.user.findFirst({
-        where : {
-            userName : username,
-            password: passwordHashed
+        where: {
+            userName: username,
         },
     })
-    
-    let res:TypeCheckUserPresentInDB
-    if(user !== null) {
-        res = {
-            isAuthenticated: true,
-            message: "User authenticated",
-            data: {
-                uid: user.uid
-            }
-        }
-        return res;
-    }else{
-        res = {
-            isAuthenticated: false,
-            message: "Invalid user",
-            data: null
-        }
-        return res;
+
+    if (!user) {
+        return null;
     }
+
+    const isPasswordCorrect = await compare(password, user.password)
+
+    if (isPasswordCorrect) {
+        return {
+            id: user.uid,
+            username: user.userName,
+            name: user.name,
+            bio: user.bio,
+            profilePic: user.profilePic
+        }
+    }
+    return null
 }
 
 
+// TODO: signup username availability
 export const checkUserPresentInDB = async (username: string) => {
     const user = await prisma.user.findFirst({
         where: {
@@ -51,35 +45,54 @@ export const checkUserPresentInDB = async (username: string) => {
     }
 }
 
+// TODO: Add return type for the function
+export const getUserInfoFromDB = async (username: string) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            userName: username
+        }
+    })
+    if (user === null) {
+        return null;
+    } else {
+        return user;
+    }
+}
+
 
 interface TypeInsertUserInDB {
-    inserted : boolean,
+    status: boolean,
     message: string
 }
+// TODO: Add the type to the parameter instead of the user as 
+// no id will be passed at the time of creating of the user 
+// from the server
 export const insertUserInDB = async (user: User) => {
     const userPresent = await checkUserPresentInDB(user.userName)
+    
     let res: TypeInsertUserInDB
+
     if (userPresent) {
         res = {
-            inserted: false,
+            status: false,
             message: "Insert Failed. Username not available"
         }
         return res
     }
-    
+
     const password = await hash(user.password, 12)
-    prisma.user.create({
-        data:{
-            uid:user.uid,
-            profilePic:user.profilePic,
-            userName:user.userName,
-            bio:user.bio,
-            password:password
+    await prisma.user.create({
+        data: {
+            name: user.name,
+            profilePic: user.profilePic,
+            userName: user.userName,
+            bio: user.bio,
+            password: password
         }
     })
 
     res = {
-        inserted: true,
+        status: true,
         message: "Inserted user into DB."
     }
     return res
