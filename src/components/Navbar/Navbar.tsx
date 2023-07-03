2e897b1cb09d83fe8b696f230a3c48a3bd7f48b6
manavkush <manavkush@gmail.com>
2023-07-03 18:00:00 +0530

    feat: Moved login page to /login - Added responsiveness to navbar - tried handling form logic at login screen
    Co-authored-by: Saloni Singh <SaloniSingh1601@users.noreply.github.com>
    
    Date:      Mon Jul 3 18:00:00 2023 +0530

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,34 @@
+"use client"
 import Link from 'next/link';
 import styles from './Navbar.module.css';
+import { useState } from 'react';
 
-const Navbar: React.FC = async () => {
+const Navbar: React.FC = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen((prevState) => !prevState);
+    };
+
     return (
-        <nav className={styles.navbar}>
+        <nav className={`${styles.navbar} ${isMobileMenuOpen ? styles.open : ''}`}>
             <div className={styles.logo}>
                 <Link className={styles.link} href="/">
-                    Logo
+                    Slapdash
                 </Link>
             </div>
             <div className={styles.links}>
-                <Link className={styles.link} href="/">
+                <Link className={`${styles.link} ${isMobileMenuOpen ? styles.open : ''}`} href="/">
                     Dashboard
                 </Link>
-                <Link className={styles.link} href="/">
+                <Link className={styles.link} href="/login">
                     Login
                 </Link>
-                <Link className={styles.link} href="/">
+                <Link className={styles.link} href="/register">
+                    Sign Up
+                </Link>
+                <Link className={styles.link} href="/team">
                     Team
                 </Link>
             </div>
