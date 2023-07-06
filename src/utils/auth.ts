a3aac022523153289a60678b8060370c0c7faeba
manavkush <manavkush@gmail.com>
2023-07-06 14:20:27 +0530

    feat: Completed authentication flow. moved nextauth options to auth.ts
    Co-authored-by: Saloni Singh <SaloniSingh1601@users.noreply.github.com>

---
+++ src/utils/auth.ts
@@ -0,0 +1,42 @@
+import { NextAuthOptions } from "next-auth";
+import { authenticateUser } from "./dbUtils";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions: NextAuthOptions = {
+    pages: {
+        signIn: "/login",
+    },
+    session: {
+        strategy: "jwt",
+    },
+    providers : [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        CredentialsProvider({
+            credentials: {
+                username: { type: "text"},
+                password: { type: "password"}
+            }, 
+            async authorize(credentials) {
+                console.log("Credentials: ", credentials)
+                let userFromDb = await authenticateUser(credentials?.username!, credentials?.password!)
+                if (!credentials?.username || !credentials.password) {
+                    throw new Error("Invalid credentials")
+                }
+                
+                let returnUser = userFromDb ? {
+                    id : userFromDb.id,
+                    username: userFromDb.username,
+                    bio: userFromDb.bio,
+                    name: userFromDb.name,
+                    profilePic: userFromDb.profilePic
+                } : null
+
+                return returnUser
+            },
+        })
+    ],
+}
