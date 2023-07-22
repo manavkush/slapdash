0bd0d97bd420ad61d6e04f8d0a27879bf311fcf5
manavkush <manavkush@gmail.com>
2023-07-22 23:41:48 +0530

    feat: Moved register route under 'user' - added update route for updating user info - fixed addChannel route variable name from addUser to addChannel
    Co-authored-by: Saloni Singh <SaloniSingh1601@users.noreply.github.com>

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -1,7 +1,11 @@
 import { Channel, PrismaClient, User } from "@prisma/client";
 import { hash, compare } from "bcrypt";
 import { profile } from "console";
-import { TypeAddChannelResponse, TypeInsertUserInDBResponse, TypeRegisterOrUpdateUserRequest } from "./types";
+import {
+    TypeAddChannelResponse,
+    TypeInsertUserInDBResponse,
+    TypeRegisterOrUpdateUserRequest,
+} from "./types";
 
 const prisma = new PrismaClient();
 
@@ -11,13 +15,13 @@ export async function authenticateUser(username: string, password: string) {
         where: {
             userName: username,
         },
-    })
+    });
 
     if (!user) {
         return null;
     }
 
-    const isPasswordCorrect = await compare(password, user.password)
+    const isPasswordCorrect = await compare(password, user.password);
 
     if (isPasswordCorrect) {
         return {
@@ -25,130 +29,135 @@ export async function authenticateUser(username: string, password: string) {
             username: user.userName,
             name: user.name,
             bio: user.bio,
-            profilePic: user.profilePic
-        }
+            profilePic: user.profilePic,
+        };
     }
-    return null
+    return null;
 }
-
 
 // TODO: signup username availability
-export const checkUsernamePresentInDB = async (username: string): Promise<boolean> => {
-
+export const checkUsernamePresentInDB = async (
+    username: string
+): Promise<boolean> => {
     const user = await prisma.user.findFirst({
         where: {
-            userName: username
-        }
-    })
+            userName: username,
+        },
+    });
 
     return !!user;
-}
+};
 
 // TODO: Add return type for the function
 export const getUserInfoFromDB = async (username: string) => {
     const user = await prisma.user.findFirst({
         where: {
-            userName: username
-        }
-    })
+            userName: username,
+        },
+    });
     if (user === null) {
         return null;
     } else {
         return user;
     }
-}
+};
 
-
-export const insertUserInDB = async (user: TypeRegisterOrUpdateUserRequest):Promise<TypeInsertUserInDBResponse> => {
-    
-    let res: TypeInsertUserInDBResponse
+export const insertUserInDB = async (
+    user: TypeRegisterOrUpdateUserRequest
+): Promise<TypeInsertUserInDBResponse> => {
+    let res: TypeInsertUserInDBResponse;
     try {
-        const password = await hash(user.password, 12)
+        const password = await hash(user.password, 12);
         await prisma.user.create({
             data: {
                 name: user.name,
                 profilePic: "",
                 userName: user.username,
                 bio: "",
-                password: password
-            }
-        })
+                password: password,
+            },
+        });
         res = {
             status: true,
-            message: "Inserted user into DB."
-        }
-    } catch(error:any) {
+            message: "Inserted user into DB.",
+        };
+    } catch (error: any) {
         res = {
             status: false,
             message: "Insert Failed. " + error.message,
-        }
+        };
     }
 
-    return res
-}
+    return res;
+};
 
-export const createNewChannelInDB = async (channelName:string): Promise<TypeAddChannelResponse> => {
-    let res: TypeAddChannelResponse
-    try{
+export const createNewChannelInDB = async (
+    channelName: string
+): Promise<TypeAddChannelResponse> => {
+    let res: TypeAddChannelResponse;
+    try {
         const newChannel: Channel = await prisma.channel.create({
             data: {
-                channelName: channelName
-            }
-        })
+                channelName: channelName,
+            },
+        });
         res = {
             status: true,
             message: "Channel created successfully",
             channelId: newChannel.id,
-            channelName: newChannel.channelName
-        }
-    } catch(error:any){
-        res={
-            status:false,
-            message: "Channel creation failed: " + error.message
-        }
+            channelName: newChannel.channelName,
+        };
+    } catch (error: any) {
+        res = {
+            status: false,
+            message: "Channel creation failed: " + error.message,
+        };
     }
-    return res
-}
+    return res;
+};
 
 interface TypeAddUserChannelConfigToDB {
-    uid: string,
-    channelId: string,
-    permission: string,
+    uid: string;
+    channelId: string;
+    permission: string;
 }
 
-interface TypeAddUserChannelConfigToDBResponse{
-    status: boolean,
-    message: string
+interface TypeAddUserChannelConfigToDBResponse {
+    status: boolean;
+    message: string;
 }
-    export const addUserChannelConfigToDB = async ({uid, channelId, permission} : TypeAddUserChannelConfigToDB): Promise<TypeAddUserChannelConfigToDBResponse> => {
-    let res: TypeAddUserChannelConfigToDBResponse
+export const addUserChannelConfigToDB = async ({
+    uid,
+    channelId,
+    permission,
+}: TypeAddUserChannelConfigToDB): Promise<TypeAddUserChannelConfigToDBResponse> => {
+    let res: TypeAddUserChannelConfigToDBResponse;
     try {
         const channelUserConfig = await prisma.channelUserConfig.upsert({
             create: {
                 uid: uid,
                 channelId: channelId,
-                permission: permission
+                permission: permission,
             },
             where: {
-                channelId_uid : {
+                channelId_uid: {
                     channelId: channelId,
-                    uid: uid
-                }
+                    uid: uid,
+                },
             },
             update: {
-                permission: permission
-            }
-        })
+                permission: permission,
+            },
+        });
         res = {
             status: true,
-            message: "channelUserConfig inserted to DB"
-        }
-        
+            message: "channelUserConfig inserted to DB",
+        };
     } catch (error: any) {
         res = {
             status: false,
-            message: error.message
-        }
+            message: error.message,
+        };
     }
     return res;
-}
+};
