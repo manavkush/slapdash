e6578e3a9f5ffe5a90c76d3f2d0a94338372aed0
manavkush <manavkush@gmail.com>
2023-07-23 23:54:39 +0530

    feat: added session check for update user info - Added 401 status to response when session not present

--- src/app/api/user/update/route.ts
+++ src/app/api/user/update/route.ts
@@ -1,8 +1,24 @@
-import { NextResponse, NextRequest } from "next/server";
+import { authOptions } from "@/src/lib/auth";
+import { TypeSession } from "@/src/utils/types";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { title } from "process";
 
 export async function POST(req: Request) {
     // TODO: Add update user info logic
-    const { title } = await req.json();
+    const session: TypeSession|null = await getServerSession(authOptions)
 
+    if(!session) {
+      // Not signedIn
+      return NextResponse.json({
+          status: 401,
+          message: {
+              error: "Authorization Error: Client not authorized to access api route."
+          }
+      }, {status: 401})
+    }
+    const updatedUser = await req.json();
+    
+    // TODO: Add dbUtil call for update
     return NextResponse.json({ title: { title } }, { status: 201 });
 }
