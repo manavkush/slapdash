eec5cd37f06e8fbaea63f7b70a459896ea05e2d8
manavkush <manavkush@gmail.com>
2023-07-24 23:57:36 +0530

    feat: Added type for update user request Added updateUserInfoInDB
    Co-authored-by: Saloni Singh <SaloniSingh1601@users.noreply.github.com>

--- src/app/api/user/update/route.ts
+++ src/app/api/user/update/route.ts
@@ -1,5 +1,6 @@
 import { authOptions } from "@/src/lib/auth";
-import { TypeSession } from "@/src/utils/types";
+import { checkUsernamePresentInDB, updateUserInfoInDB } from "@/src/utils/dbUtils";
+import { TypeSession, TypeUpdateUserRequest } from "@/src/utils/types";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { title } from "process";
@@ -7,7 +8,7 @@ import { title } from "process";
 export async function POST(req: Request) {
     // TODO: Add update user info logic
     const session: TypeSession|null = await getServerSession(authOptions)
-
+    
     if(!session) {
       // Not signedIn
       return NextResponse.json({
@@ -17,7 +18,10 @@ export async function POST(req: Request) {
           }
       }, {status: 401})
     }
-    const updatedUser = await req.json();
+    const uid = session.user.id
+    const updatedUser: TypeUpdateUserRequest = await req.json();
+
+    updateUserInfoInDB(updatedUser, uid)
     
     // TODO: Add dbUtil call for update
     return NextResponse.json({ title: { title } }, { status: 201 });
