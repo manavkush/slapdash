a05cfb704f978c91317937bf99331cd8e063e96c
manavkush <manavkush@gmail.com>
2023-07-31 21:25:11 +0530

    chore: Testing the session on client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,21 @@ import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import styles from './page.module.css'
 import { Button, Input } from 'reactstrap'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useSession } from 'next-auth/react';
+import { TypeSession } from '../utils/types';
+import { authOptions } from '../lib/auth';
 // import Navbar from '../components/Navbar/Navbar';
 
 export default function Home() {
-  return (
-    <div className={styles.home}>
+  const session = useSession();
+  
+
+  return session.data !== undefined ? (
+    <div style={{color:"white"}} className={styles.home}>
+      Dashboard SignedIn
+    </div>
+  ) : (
+    <div style={{color:"white"}} className={styles.home}>
       Dashboard
     </div>
   )
