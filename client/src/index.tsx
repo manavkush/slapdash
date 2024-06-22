9298c9e97a06bf5e39377a97d33309e6d994af29
manavkush <manavkush@gmail.com>
2024-06-22 17:19:05 +0530

    feat: Added changes to app component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,26 +3,6 @@ import { render } from 'solid-js/web'
 
 import './index.css'
 import App from './App'
-import { createResource } from 'solid-js'
-
-function Layout() {
-  const [user] = createResource(getUser)
-
-  // Checks the backend if the user is logged-in
-  async function getUser() {
-    const response = await fetch("http://localhost:3000/getUser", {
-      credentials: 'include'
-    })
-    const responseJson = await response.json()
-    if (responseJson.error == "1") {
-      return null
-    }
-
-    responseJson.user = JSON.parse(responseJson.user)
-    return responseJson
-  }
-
-}
 
 const root = document.getElementById('root')
 render(() => (
