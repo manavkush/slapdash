ce1f23b118410b3543eeb70d4c093cc7eb4c25b9
manavkush <manavkush@gmail.com>
2024-06-29 14:46:17 +0530

    refactor: refactored the dashboard

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -3,6 +3,7 @@ import { Accessor, Setter } from "solid-js";
 interface propsType { currentChat: Accessor<null | string>, setCurrentChat: Setter<null | string> }
 
 export function ChatWindow(props: propsType) {
+  console.log("Current Chat", props.currentChat)
   return <div class="w-3/4">
     Chat Window
   </div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -10,33 +10,29 @@ export const Navbar: ParentComponent<{}> = (props) => {
     <>
       <nav class={`w-full h-24 md:h-16 flex md:flex-row flex-col justify-between fixed p-4 top-0 left-0 bg-gradient-to-r from-[#000000] to-[#222222] text-[#ffffff]`}>
         <div>
-          <A class='text-[#ffffff] text-2xl hover:text-[#c8c8c3]' href="/" >
+          <A class='text-[#ffffff] text-2xl flex text-center justify-center justify-items-center self-center hover:text-[#c8c8c3]' href="/" >
             Slapdash
           </A>
         </div>
 
         <div class={`flex align-middle justify-center`}>
-          <A class="text-[#ffffff] text-xl hover:text-[#c8c8c3] text-center flex pl-4" href="/">
+          <A class="text-[#ffffff] text-l hover:text-[#c8c8c3] text-center flex pl-4" href="/">
             Dashboard
           </A>
 
           <Show when={user() == undefined}>
-            <A class="text-[#ffffff] text-xl hover:text-[#c8c8c3] pl-4" href="http://localhost:3000/auth/google">
+            <A class="text-[#ffffff] text-l hover:text-[#c8c8c3] pl-4" href="http://localhost:3000/auth/google">
               Login
-            </A>
-
-            <A class="text-[#ffffff] text-xl hover:text-[#c8c8c3] pl-4" href="/register">
-              Sign Up
             </A>
           </Show>
 
           <Show when={user() != undefined} >
-            <A class="text-[#ffffff] text-xl hover:text-[#c8c8c3] pl-4" href='http://localhost:3000/auth/logout/google'>
+            <A class="text-[#ffffff] text-l hover:text-[#c8c8c3] pl-4" href='http://localhost:3000/auth/logout/google'>
               Logout
             </A>
           </Show>
 
-          <A class="text-[#ffffff] text-xl hover:text-[#c8c8c3] pl-4" href="/team">
+          <A class="text-[#ffffff] text-l hover:text-[#c8c8c3] pl-4" href="/team">
             Team
           </A>
         </div>
